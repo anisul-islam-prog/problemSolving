44f7e06a5d09c34e94e1bdbe77ad100c8748f0b4
anisul-islam-prog <kingfahad0802@gmail.com>
2022-05-17 05:42:42 +0600

    Day 2

---
+++ Valid Anagram/Solution1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        
+        if len(s) != len(t):
+            return False
+        
+        # Declaring hasmap to store char count of S & T
+        countS , countT = {}, {} 
+        
+        # Count Number of characters in S & T
+        for i in range(len(s)):
+            # .get for fetching value against key and 0 is default if key does not exists
+            countS[s[i]]= 1+countS.get(s[i], 0) 
+            countT[t[i]]= 1+countT.get(t[i], 0)
+        # Comparing char count of S & T
+        for c in countS:
+            if countS[c] != countT.get(c, 0):
+                return False
+            
+        return True
